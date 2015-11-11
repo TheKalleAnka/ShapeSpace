9c147751d490c86414f655c6651144e29bbfc4f1
TheKalleAnka <marcus.bjors@hotmail.com>
2015-11-11 15:57:13 +0100

    2015-11-11
    Serer

--- ShapeSpaceServer/Program.cs
+++ ShapeSpaceServer/Program.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+using Lidgren.Network;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        NetPeerConfiguration config = new NetPeerConfiguration("ShapeSpace");
+        config.MaximumConnections = 10;
+        config.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+
+        NetServer server = new NetServer(config);
+
+        try
+        {
+            server.Start();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+            WaitForKeyPress();
+        }
+
+        while(true)
+        {
+            //Message handling loop
+            NetIncomingMessage msg;
+            while ((msg = server.ReadMessage()) != null)
+            {
+                switch (msg.MessageType)
+                {
+                    case NetIncomingMessageType.DiscoveryRequest:
+                        break;
+                    default:
+                        Console.WriteLine("Unhandled type: " + msg.MessageType);
+                        break;
+                }
+                server.Recycle(msg);
+            }
+        }
+    }
+
+    static void WaitForKeyPress()
+    {
+        Console.Write("Press any key to continue...");
+        Console.ReadKey(true);
     }
 }
